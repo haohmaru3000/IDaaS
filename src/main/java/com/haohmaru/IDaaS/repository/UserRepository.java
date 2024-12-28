71aa1960c884c9ae7f5749f050796a295e83715a
haohmaru3000 <tuanit168@gmail.com>
2024-12-28 11:59:30 +0700

    4. Basic CRUD APIs

---
+++ src/main/java/com/haohmaru/IDaaS/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.haohmaru.IDaaS.repository;
+
+import com.haohmaru.IDaaS.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
---
+++ src/main/java/com/haohmaru/IDaaS/service/UserService.java
@@ -0,0 +1,52 @@
+package com.haohmaru.IDaaS.service;
+
+import com.haohmaru.IDaaS.dto.request.UserCreationRequest;
+import com.haohmaru.IDaaS.dto.request.UserUpdateRequest;
+import com.haohmaru.IDaaS.entity.User;
+import com.haohmaru.IDaaS.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreationRequest request) {
+        // Map User info from Request into User model
+        User user = new User();
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user); // Create a new row in table
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request) {
+        User user = getUser(userId);
+
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
